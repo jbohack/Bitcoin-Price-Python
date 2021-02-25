d08b6b1d50d97992850d2a7b92d2a975e7bf42b2
Joe Bohack <joebohack@gmail.com>
2021-02-25 11:26:40 -0500

    Added configuration for logging data to a file per interval

--- btc_price.py
+++ btc_price.py
@@ -5,11 +5,19 @@ import time
 import threading
 
 
-version = "v1.4"
+version = "v1.5"
 print("---------------------\n", version, "Bitcoin to USD\n---------------------")
 
+# enable / disable logging to a file completly
 logDataToFile = "true"
 
+# enable / disable logging data to a log file per interval
+logData_65Seconds = "true" 
+logData_5Minutes = "true"  
+logData_10Minutes = "true"
+logData_30Minutes = "true"
+
+# you can modify the file names for the logs here
 fileName_65Seconds = "bitcoin_price_data_65_seconds"
 fileName_5Minutes = "bitcoin_price_data_5_minutes"
 fileName_10Minutes = "bitcoin_price_data_10_minutes"
@@ -24,7 +32,7 @@ def refreshData_65Seconds():
         print ("1 Bitcoin ~= $" + str(response_65Seconds.text) + " USD |" + time.ctime() + "|")
         print("Waiting 65 seconds before requesting information again..")
 
-        if logDataToFile == "true":
+        if logDataToFile == "true" and logData_65Seconds == "true":
             with open(os.path.join(sys.path[0], fileName_65Seconds + ".log"), "a") as log:
                 log.write("1 Bitcoin ~= $" + str(response_65Seconds.text) + " USD |" + time.ctime() + "|\n")
             time.sleep(65)
@@ -37,7 +45,7 @@ def refreshData_5Minutes():
         print ("1 Bitcoin ~= $" + str(response_5Minutes.text) + " USD |" + time.ctime() + "|")
         print("Waiting 5 minutes before requesting information again..")
 
-        if logDataToFile == "true":
+        if logDataToFile == "true" and logData_5Minutes == "true":
             with open(os.path.join(sys.path[0], fileName_5Minutes + ".log"), "a") as log:
                 log.write("1 Bitcoin ~= $" + str(response_5Minutes.text) + " USD |" + time.ctime() + "|\n")
             time.sleep(300)
@@ -50,7 +58,7 @@ def refreshData_10Minutes():
         print ("1 Bitcoin ~= $" + str(response_10Minutes.text) + " USD |" + time.ctime() + "|")
         print("Waiting 10 minutes before requesting information again..")
 
-        if logDataToFile == "true":
+        if logDataToFile == "true" and logData_10Minutes == "true":
             with open(os.path.join(sys.path[0], fileName_10Minutes + ".log"), "a") as log:
                 log.write("1 Bitcoin ~= $" + str(response_10Minutes.text) + " USD |" + time.ctime() + "|\n")
             time.sleep(600)
@@ -63,7 +71,7 @@ def refreshData_30Minutes():
         print ("1 Bitcoin ~= $" + str(response_30Minutes.text) + " USD |" + time.ctime() + "|")
         print("Waiting 30 minutes before requesting information again..")
 
-        if logDataToFile == "true":
+        if logDataToFile == "true" and logData_30Minutes == "true":
             with open(os.path.join(sys.path[0], fileName_30Minutes + ".log"), "a") as log:
                 log.write("1 Bitcoin ~= $" + str(response_30Minutes.text) + " USD |" + time.ctime() + "|\n")
             time.sleep(1800)
